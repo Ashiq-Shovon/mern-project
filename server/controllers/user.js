7742cc7206e0124d18e4ce8d626b472ca263e993
unknown <ashiqshovon39@gmail.com>
2022-08-13 06:00:51 -0700

    mongoose model is included in user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,30 @@
 
+const mongoose = require('mongoose');
 var userMiddle = require('../middleware/middle.js');
-exports.User = function(req,res){
+var userSchema = require('../schemas/userSchema');
+const user = new mongoose.model('user',userSchema);
+
+exports.User = async (req,res) =>{
     
     
     res.send('respond with a resou');
     
   
+}
+
+exports.userCreate = async (req, res) => {
+    const newUser = new user(req.body);
+    await user.save((err) => {
+        if(err){
+            res.status(500).json({
+                error:"There was a server side error",
+            });
+        }else{
+            res.status(200).json({
+                message:"User was inserted successfully",
+            });
+        }
+    });
 }
 
 
