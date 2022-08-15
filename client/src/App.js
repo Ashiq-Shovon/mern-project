08df462efe362e90a70440511515af445059acc2
unknown <ashiqshovon39@gmail.com>
2022-08-15 12:26:15 -0700

    transfer router code from app.js to spa.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
-import ReactDom from 'react-dom';
+// import ReactDom from 'react-dom';
 import Home from "./pages/home/Home.js";
-import Login from "./pages/login/Login.js";
-import List from "./pages/list/List.js";
-import New from "./pages/new/New.js";
-import Single from "./pages/single/Single.js";
+// import Login from "./pages/login/Login.js";
+// import List from "./pages/list/List.js";
+// import New from "./pages/new/New.js";
+// import Single from "./pages/single/Single.js";
+import UserList from './components/main/UserList.js';
 import Spa from './components/Spa.js';
 import "./App.css";
 import {
@@ -13,24 +14,25 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import {FiSettings} from 'react-icons/fi';
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { Calender} from "@syncfusion/ej2-calendars";
+// import {FiSettings} from 'react-icons/fi';
+// import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+// import { Calender} from "@syncfusion/ej2-calendars";
 
-import { ScheduleComponent, ViewsDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop }
-from "@syncfusion/ej2-react-schedule";
+// import { ScheduleComponent, ViewsDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop }
+// from "@syncfusion/ej2-react-schedule";
  
 // import {DatePickerComponent} from "@syncfusion/ej2-react-calenders";
 
 function App() {
-  const activeMenu = false;
+  // const activeMenu = false;
   return (
     <div>
+       <Spa />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home/>}></Route>
-            <Route path="login" element={<Login/>}></Route>
+            <Route path="user-list" element={<UserList/>}></Route>
             <Route path="users">
               <Route index element={<List/>}></Route>
               <Route path="new" element={<New/>}></Route>
@@ -46,7 +48,7 @@ function App() {
         </Routes>
       </BrowserRouter> */}
       {/* <BrowserRouter> */}
-        <Spa />
+       
        
       
   
